73d0425cb9c27b25e224321076349b7582cefbe8
Sebastin Gonzlez <sebas1493@ubuntu.(none)>
2013-02-25 17:23:17 -0500

    Se cambiaron los parametros de la funcion "agregarlibro" y de "agregarrevista"

--- coleccion/Coleccion.hh
+++ coleccion/Coleccion.hh
@@ -13,8 +13,8 @@ class Coleccion
 		vector <Libro> libr [7];
         string materias [7] = {"fisica", "matematicas", "economia", "computacion", "entretenimiento", "novedades", "cocina";
         vector <Revistas> revist [7];
-		void agregarLibro(Libro l);
-		void agregarrevista(Revista r);
+		void agregarLibro(string nombre,int edicion,string autor,int id,string genero);
+		void agregarrevista(string nombre,int Id,int edicion,string materia,string fecha);
 		void eliminarLibro(Libro l);
 		void eliminarRevista(Revista r);
 		bool buscarLibro(string nombre,int edicion,string autor,int id,string genero);
